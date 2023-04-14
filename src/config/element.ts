534a09598a3a1965a185c5b1d45934a4757c3a11
plumeink <hansion@plumeink.com>
2023-04-15 02:25:20 +0800

    init

---
+++ src/config/element.ts
@@ -0,0 +1,4 @@
+export default {
+    zIndex: 3000,
+    size: 'small'
+}
---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import path from 'path'
+import {fileURLToPath, URL} from 'node:url'
+
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        vue(),
+        vueJsx(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver({
+                importStyle: "sass",
+            })],
+        }),
+    ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@use "~/styles/element/index.scss" as *;`,
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            '~/': `${path.resolve(__dirname, 'src')}/`
+        },
+    }
+})
